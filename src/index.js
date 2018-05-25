dbf2c0084771dc89f865260cb149964c2c89b228
hyojin.sarchet <hyojin5931@hanmail.net>
2018-05-25 15:15:33 -0400

    created more components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+
+// Learned about the BrowserRouter from the link below:
+// https://medium.com/@pshrmn/a-simple-react-router-v4-tutorial-7f23ff27adf
